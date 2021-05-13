5943051631a0d77f6061294a9844af1d2d40abaf
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-05-13 20:00:56 +0200

    finish step 6

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ const Button = ({ text, ...restProps }) => (
 )
 
 const Statistic = ({ text, value }) => (
-  <div>
-    {text} {value}
-  </div>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -20,14 +21,16 @@ const Statistics = ({ good, neutral, bad }) => {
   }
 
   return (
-    <div>
-      <Statistic text="good" value={good} />
-      <Statistic text="neutral" value={neutral} />
-      <Statistic text="bad" value={bad} />
-      <Statistic text="all" value={all} />
-      <Statistic text="average" value={`${average} %`} />
-      <Statistic text="positive" value={`${positive} %`} />
-    </div>
+    <table>
+      <tbody>
+        <Statistic text="good" value={good} />
+        <Statistic text="neutral" value={neutral} />
+        <Statistic text="bad" value={bad} />
+        <Statistic text="all" value={all} />
+        <Statistic text="average" value={`${average} %`} />
+        <Statistic text="positive" value={`${positive} %`} />
+      </tbody>
+    </table>
   )
 }
 
