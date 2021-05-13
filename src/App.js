73e14fe1dc4b5d14398866c9f7454677a5c07889
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-05-13 19:56:01 +0200

    finish step 4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good - bad) / all
   const positive = (good / all) * 100
 
+  if (all === 0) {
+    return <div>No feedback given yet.</div>
+  }
+
   return (
     <div>
       <div>good {good}</div>
@@ -21,9 +25,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
-  const average = (good - bad) / all
-  const positive = (good / all) * 100
 
   return (
     <div>
