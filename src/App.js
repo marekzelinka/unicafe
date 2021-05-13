2e64cc15812684150ad3f784ebbdfe008524d1b9
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-05-13 19:44:10 +0200

    finish step 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from 'react'
+
 const App = () => {
-  return <div></div>
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <button onClick={() => setGood((good) => good + 1)}>good</button>
+      <button onClick={() => setNeutral((neutral) => neutral + 1)}>
+        neutral
+      </button>
+      <button onClick={() => setBad((bad) => bad + 1)}>bad</button>
+      <h2>statistics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  )
 }
 
 export default App
